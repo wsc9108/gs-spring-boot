cff9ddcb7c7a15c9ef02f334117e10a5ef552193
wsc9108 <wsc9108@163.com>
2019-07-24 22:03:46 +0800

    add 123

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -19,7 +19,7 @@ public class Application {
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
 
-            System.out.println("Let's inspect the beans provided by Spring Boot on Azure");
+            System.out.println("Let's inspect the beans provided by Spring Boot on Azure123");
 
             String[] beanNames = ctx.getBeanDefinitionNames();
             Arrays.sort(beanNames);
