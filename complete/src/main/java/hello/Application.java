5f0162dbff3d41cf091975cb3a71b7db2323e053
wsc9108 <wsc9108@163.com>
2019-07-24 22:07:40 +0800

    add 456

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -19,7 +19,7 @@ public class Application {
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
 
-            System.out.println("Let's inspect the beans provided by Spring Boot on Azure123");
+            System.out.println("Let's inspect the beans provided by Spring Boot on Azure123 456");
 
             String[] beanNames = ctx.getBeanDefinitionNames();
             Arrays.sort(beanNames);
